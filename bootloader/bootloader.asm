4af20211e0174e002896454fa763cc900ef607bd
David Lightman <david-lightman@adelphos.org>
2020-05-24 22:32:40 -0400

    added comments

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -1,32 +1,36 @@
 bits 16
 start:
-mov ax, 0x07c0
-add ax, 0x20
-mov ss, ax
-mov sp, 4096
+mov ax, 0x07c0  ;512B MBR loaded to 0x07c00
+add ax, 0x20    ;offset for 0x200 (512 bytes)
+mov ss, ax      ;move the bottom of the stack to 0x07e00 (0x7c00+0x200 bytes)
+mov sp, 0x1000  ;stack pointer to 0x08e00 (stack grows down towards ss)
 
-mov ax, 0x07c0
-mov ds, ax
+mov ax, 0x07c0  ;set the data segment
+mov ds, ax      ;
 
-mov si, msg
-call print
+mov si, msg     ;place ptr to our msg in source-index
+call print      ; goto print:
 cli
 hlt
 data:
- msg db 'Hello bootloader!', 0
+ msg db 'Hello bootloader!', 0  ; copy string to memory location in data-seg
 
 print:
- mov ah, 0x0e 
+ mov ah, 0x0e   ; move the print-char BIOS procedure into AH
+                ; fall thru to .printchar
 
 .printchar:
- lodsb
- cmp al, 0
- je .done
- int 0x10
- jmp .printchar
+ lodsb          ;load a byte from SI into AL and increment SI
+ cmp al, 0      ; compare to zero
+ je .done       ; jump if above is equal to zero
+ int 0x10       ; 10H is BIOS video services; 0x0e is write char in TTY mode
+ jmp .printchar ; do it again from .printchar
 
 .done:
  ret
 
+; MBR is 512 bytes
+; zero out 510 bytes (from the beginning of section ($$) to current address ($))
 times 510 - ($ - $$) db 0
-dw 0xaa55
+; boot signature - some older BIOSes use this to ID a boot sector
+dw 0xaa55   
